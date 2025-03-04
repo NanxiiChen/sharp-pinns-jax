fix bugs in causal

example/two_d_two_pit/main-stagger.py
@@ -34,7 +34,7 @@ class PINN(nn.Module):
                               self.loss_irr, self.loss_flux]
         self.pde_name = "ac"
         self.aux_vars = {}
-        self.causal_weightor = CausalWeightor(num_chunks=CHUNKS, t_range=(0.0, 1.0))
+        self.causal_weightor = CausalWeightor(num_chunks=CHUNKS, t_range=(0.0, 1.0), pde_name="ac")
         arch = {"mlp": MLP, "modified_mlp": ModifiedMLP}
         self.model = arch[arch_name](act_name=act_name, num_layers=num_layers,
                                      hidden_dim=hidden_dim, out_dim=out_dim, fourier_emb=fourier_emb)
@@ -181,7 +181,7 @@ class PINN(nn.Module):
     @partial(jit, static_argnums=(0,))
     def net_speed(self, params, x, t):
         jac_dt = jax.jacrev(self.net_u, argnums=2)
-        dphi_dt, dc_dt = jac_dt(params, x, t)
+        dphi_dt, dc_dt = jac_dt(params, x, t) * Tc
         return dphi_dt, dc_dt
     
     @partial(jit, static_argnums=(0,))
@@ -209,7 +209,7 @@ class PINN(nn.Module):
         return jnp.mean(ch ** 2)
     
     @partial(jit, static_argnums=(0,))
-    def loss_pde(self, params, batch):
+    def loss_pde(self, params, batch, eps):
         x, t = batch
         pde_name = self.pde_name
         pde_fn = self.net_ac if pde_name == "ac" else self.net_ch
@@ -217,7 +217,7 @@ class PINN(nn.Module):
         if not CAUSAL_WEIGHT:
             return jnp.mean(res ** 2)
         else:
-            return self.causal_weightor.compute_causal_loss(res, t)
+            return self.causal_weightor.compute_causal_loss(res, t, eps)
 
     @partial(jit, static_argnums=(0,))
     def loss_ic(self, params, batch):
@@ -245,7 +245,7 @@ class PINN(nn.Module):
         return jnp.mean(dphi_dy ** 2) + jnp.mean(dc_dy ** 2)
         
     @partial(jit, static_argnums=(0,))
-    def compute_losses_and_grads(self, params, batch):
+    def compute_losses_and_grads(self, params, batch, eps):
         if len(batch) != len(self.loss_fn_panel):
             raise ValueError("The number of loss functions "
                              "should be equal to the number of items in the batch")
@@ -255,7 +255,7 @@ class PINN(nn.Module):
         for idx, (loss_item_fn, batch_item) in enumerate(zip(self.loss_fn_panel, batch)):
             if idx == 0:
                 (loss_item, aux), grad_item = jax.value_and_grad(
-                    loss_item_fn, has_aux=True)(params, batch_item)
+                    loss_item_fn, has_aux=True)(params, batch_item, eps)
                 aux_vars.update(aux)
             else:
                 loss_item, grad_item = jax.value_and_grad(
@@ -266,8 +266,8 @@ class PINN(nn.Module):
         return jnp.array(losses), grads, aux_vars
 
     @partial(jit, static_argnums=(0,))
-    def loss_fn(self, params, batch,):
-        losses, grads, aux_vars = self.compute_losses_and_grads(params, batch)
+    def loss_fn(self, params, batch, eps):
+        losses, grads, aux_vars = self.compute_losses_and_grads(params, batch, eps)
 
         weights = self.grad_norm_weights(grads)
         weights = jax.lax.stop_gradient(weights)
@@ -313,8 +313,8 @@ class Sampler:
         return adaptive_base[indices]
 
     def sample_pde(self):
-        data = lhs_sampling(self.mins, self.maxs, self.n_samples**3)
-        # data = shfted_grid(self.mins, self.maxs, [self.n_samples, self.n_samples, self.n_samples*2], self.key)
+        # data = lhs_sampling(self.mins, self.maxs, self.n_samples**3)
+        data = shfted_grid(self.mins, self.maxs, [self.n_samples, self.n_samples, self.n_samples*3], self.key)
         return data[:, :-1], data[:, -1:]
 
     def sample_ac(self):
@@ -430,10 +430,10 @@ def create_train_state(model, rng, lr, **kwargs):
 
 
 @jit
-def train_step(state, batch):
+def train_step(state, batch, eps):
     params = state.params
     (weighted_loss, (loss_components, weight_components, aux_vars)), grads = jax.value_and_grad(
-        pinn.loss_fn, has_aux=True, argnums=0)(params, batch)
+        pinn.loss_fn, has_aux=True, argnums=0)(params, batch, eps)
     new_state = state.apply_gradients(grads=grads)
     return new_state, (weighted_loss, loss_components, weight_components, aux_vars)
 
@@ -475,6 +475,7 @@ start_time = time.time()
 for epoch in range(EPOCHS):
     pde_name = "ac" if (epoch % PAUSE_EVERY) < (PAUSE_EVERY // 2) else "ch"
     pinn.pde_name = pde_name
+    pinn.causal_weightor.pde_name = pde_name
     # pinn.causal_weightor = causal_weightor_ac if pde_name == "ac" else causal_weightor_ch
     # pde_name = "ch" if (epoch % PAUSE_EVERY) < (PAUSE_EVERY // 2) else "ac"
     # pinn.loss_fn_panel = [
@@ -489,8 +490,8 @@ for epoch in range(EPOCHS):
         sampler.adaptive_kw["params"].update(state.params)
         batch = sampler.sample(pde_name=pde_name)
     state, (weighted_loss, loss_components,
-            weight_components, aux_vars) = train_step(state, batch)
-    pinn.causal_weightor.update_causal_configs(aux_vars["causal_weights"])
+            weight_components, aux_vars) = train_step(state, batch, CAUSAL_CONFIGS[pde_name + "_eps"])
+    update_causal_eps(aux_vars["causal_weights"], CAUSAL_CONFIGS, pde_name)
     if epoch % (PAUSE_EVERY//2) == 0:
         fig, error = evaluate2D(
             pinn, state.params,
@@ -521,7 +522,8 @@ for epoch in range(EPOCHS):
         
         fig = pinn.causal_weightor.plot_causal_info(pde_name, 
                                                     aux_vars["causal_weights"],
-                                                    aux_vars["loss_chunks"])
+                                                    aux_vars["loss_chunks"],
+                                                    CAUSAL_CONFIGS[pde_name + "_eps"])
         metrics_tracker.register_figure(epoch, fig, "causal_info")
         plt.close(fig)
         

pf_pinn/arch.py
@@ -94,7 +94,7 @@ class ModifiedMLP(nn.Module):
 
         if self.fourier_emb:
             # separate the spatial and temporal coordinates
-            t_emb = FourierEmbedding(emb_scale=2.0/4)(t)
+            t_emb = FourierEmbedding(emb_scale=2.0)(t)
             x_emb = FourierEmbedding(emb_scale=2.0)(x)
             x = jnp.concatenate([x_emb, t_emb], axis=-1)
         else:
@@ -107,7 +107,7 @@ class ModifiedMLP(nn.Module):
 
         for _ in range(self.num_layers):
             x = Dense(x.shape[-1], self.hidden_dim)(x)
-            x = self.act_fn(x)
+            x = nn.tanh(x)
             x = x * u + (1 - x) * v
 
         return Dense(self.hidden_dim, self.out_dim)(x)

pf_pinn/causal.py
@@ -11,63 +11,37 @@ import matplotlib.pyplot as plt
 
 
 class CausalWeightor:
-    def __init__(self, num_chunks: int, t_range: tuple):
-        self.causal_configs = {
-            "eps": 1e-3,
-            "step_size": 10,
-            "max_last_weight": 0.99,
-            "min_mean_weight": 0.5,
-            "max_eps": 1e-3
-        }
+    def __init__(self, num_chunks: int, t_range: tuple, pde_name="ac"):
+
         self.num_chunks = num_chunks
         self.t_range = t_range
         self.bins = jnp.linspace(t_range[0], t_range[1], num_chunks+1)
+        self.pde_name = pde_name
     
-    
-    def update_causal_configs(self, causal_weights):
 
-        if causal_weights.min() > self.causal_configs["max_last_weight"] \
-            and self.causal_configs["eps"] < self.causal_configs["max_eps"]:
-                self.causal_configs["eps"] *= self.causal_configs["step_size"]
-                # print(f"Inc. eps to {self.causal_configs['eps']}")
-        
-        if jnp.mean(causal_weights) < self.causal_configs["min_mean_weight"]:
-            self.causal_configs["eps"] /= self.causal_configs["step_size"]
-            # print(f"Dec. eps to {self.causal_configs['eps']}")
-    
-    # def _update_causal_configs(self):
-    #     eps_init = self.causal_configs["eps"]
-    #     causal_weights = self.causal_weights
-        
-    #     cond1 = (causal_weights.min() > self.causal_configs["max_last_weight"]) & (eps_init < self.causal_configs["max_eps"])
-    #     eps_after_cond1 = jnp.where(cond1, eps_init * self.causal_configs["step_size"], eps_init)
+            
 
-    #     cond2 = jnp.mean(causal_weights) < self.causal_configs["min_mean_weight"]
-    #     new_eps = jnp.where(cond2, eps_after_cond1 / self.causal_configs["step_size"], eps_after_cond1)
+    # def _split_t(self, t: jnp.array, ):
+    #     num = self.num_chunks
+    #     bins = self.bins
+    #     t_idx = jnp.digitize(t, bins) - 1
 
-    #     self.causal_configs.update({"eps": new_eps})
+    #     return [jnp.where(t_idx == int(i))[0] for i in range(num)]
 
-
-    def _split_t(self, t: jnp.array, ):
-        num = self.num_chunks
-        bins = self.bins
-        t_idx = jnp.digitize(t, bins) - 1
-
-        return [jnp.where(t_idx == int(i))[0] for i in range(num)]
-
-    def _compute_causal_weight(self, loss_chunks: jnp.array):
-        eps = self.causal_configs["eps"]
-        weights = jnp.zeros(self.num_chunks)
+    # @partial(jax.jit, static_argnums=(0,))
+    def compute_causal_weight(self, loss_chunks: jnp.array, eps: jnp.array):
+        weights = []
         for chunk_id in range(self.num_chunks):
             if chunk_id == 0:
-                weights = weights.at[chunk_id].set(1.0)
+                weights.append(1.0)
             else:
-                weights = weights.at[chunk_id].set(jnp.exp(
+                
+                weights.append(jnp.exp(
                     -eps * jnp.sum(loss_chunks[:chunk_id])
                 ))
-        weights = jax.lax.stop_gradient(weights)
+
+        return jax.lax.stop_gradient(jnp.array(weights))
         # self.causal_weights = jax.device_get(weights)
-        return weights
 
     # @partial(jax.jit, static_argnums=(0,))
     # def compute_causal_loss(self, residuals: jnp.array, t: jnp.array):
@@ -81,26 +55,25 @@ class CausalWeightor:
     #     self.caual_loss = causal_loss
     #     return causal_loss
     
-    def compute_causal_loss(self, residuals: jnp.array, t: jnp.array):
-        # 1. 计算每个样本所在的分段编号
+    # @partial(jax.jit, static_argnums=(0,))
+    def compute_causal_loss(self, residuals: jnp.array, t: jnp.array, eps: jnp.array):
         t_idx = jnp.digitize(t.flatten(), self.bins) - 1
-        
 
-        sum_residuals_sq = jax.ops.segment_sum(residuals**2, t_idx, num_segments=self.num_chunks)
-        count_residuals = jax.ops.segment_sum(jnp.ones_like(residuals), t_idx, num_segments=self.num_chunks)
+        sum_residuals_sq = jax.ops.segment_sum(residuals**2, \
+            t_idx, num_segments=self.num_chunks)
+        count_residuals = jax.ops.segment_sum(jnp.ones_like(residuals), \
+            t_idx, num_segments=self.num_chunks)
         loss_chunks = sum_residuals_sq / (count_residuals + 1e-12)
         
-        causal_weights = self._compute_causal_weight(loss_chunks)
+        causal_weights = self.compute_causal_weight(loss_chunks, eps)
         causal_loss = jnp.dot(loss_chunks, causal_weights)
-        # self.aux_vars["causal_weights"] = jax.device_get(causal_weights)
-        # self.aux_vars["loss_chunks"] = jax.device_get(loss_chunks)
         return causal_loss, {
             "causal_weights": causal_weights,
             "loss_chunks": loss_chunks
         }
     
     
-    def plot_causal_info(self, pde_name, causal_weights, loss_chunks):
+    def plot_causal_info(self, pde_name, causal_weights, loss_chunks, eps):
         
         bins = (self.bins[1:] + self.bins[:-1]) / 2
         
@@ -114,12 +87,37 @@ class CausalWeightor:
         ax.plot(bins, loss_chunks, marker="o")
         ax.set(xlabel="Time chunks", ylabel="Causal loss", title=f"Causal loss for {pde_name}")
         
+        fig.suptitle(f"EPS: {eps:.2e}")
+        
         return fig
         
         
-
         
+def update_causal_eps(causal_weight, causal_configs, pde_name):
     
+    if causal_weight.min() > causal_configs["max_last_weight"] \
+        and causal_configs[pde_name + "_eps"] < causal_configs["max_eps"]:
+            causal_configs[pde_name + "_eps"] *= causal_configs["step_size"]
+            print(f"Inc. eps to {causal_configs[pde_name + '_eps']}")
+            
+    if jnp.mean(causal_weight) < causal_configs["min_mean_weight"]:
+        causal_configs[pde_name + "_eps"] /= causal_configs["step_size"]
+        print(f"Dec. eps to {causal_configs[pde_name + '_eps']}")
+            
+    
+    
+    # @partial(jax.jit, static_argnums=(0,))
+    # def update_causal_configs(self, causal_weights: jnp.array):
+    #     cfgs = self.causal_configs[self.pde_name]
+
+    #     if causal_weights.min() > cfgs["max_last_weight"] \
+    #         and cfgs["eps"] < cfgs["max_eps"]:
+    #             cfgs["eps"] *= cfgs["step_size"]
+    #             print(f"Inc. eps to {cfgs['eps']}")
+        
+    #     if jnp.mean(causal_weights) < cfgs["min_mean_weight"]:
+    #         cfgs["eps"] /= cfgs["step_size"]
+    #         print(f"Dec. eps to {cfgs['eps']}")
 
 if __name__ == "__main__":
     t = jnp.array([0.0, 0.1, 0.7, 0.9, 0.3, 0.5, 1.0]).reshape(-1, 1)