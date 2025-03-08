abe7b7aac4081cc690221e5d62317aee5414481f
Nanxi Chen <nxchen@tongji.edu.cn>
2025-03-08 20:48:48 +0800

    update sample methods

--- pf_pinn/sample.py
+++ pf_pinn/sample.py
@@ -1,6 +1,8 @@
+import jax 
+
+from functools import partial
 from jax import random
 import jax.numpy as jnp
-from pyDOE import lhs
 import matplotlib.pyplot as plt
 
 
@@ -10,52 +12,85 @@ def mesh_flat(*args):
     ]
 
 
-def lhs_sampling(mins, maxs, num):
-    lb = jnp.array(mins)
-    ub = jnp.array(maxs)
-    if not len(lb) == len(ub):
-        raise ValueError(f"mins and maxs should have the same length.")
-    ret = lhs(len(lb), int(num)) * (ub - lb) + lb
-    # return [ret[:, :-1], ret[:, -1:]]
-    return ret
+def lhs_sampling(mins, maxs, num, key):
+    dim = len(mins)
+    u = (jnp.arange(0, num) + 0.5) / num  # 每个区间的中点
+    
+    keys = random.split(key, dim)
+    result = jnp.zeros((num, dim))
+    
+    for i in range(dim):
+        perm = random.permutation(keys[i], u)
+        result = result.at[:, i].set(mins[i] + perm * (maxs[i] - mins[i]))
+    
+    return result
 
-# in Torch
-# def make_uniform_grid_data_transition(mins, maxs, num):
+def shifted_grid(mins, maxs, nums, key):
+    dim = len(mins)
+    mins = jnp.array(mins)
+    maxs = jnp.array(maxs)
+    nums = jnp.array(nums)
+    
+    grids = []
+    distances = (maxs - mins) / (nums - 1)
+    
+    keys = random.split(key, dim)
+    shifts = jnp.array([random.uniform(keys[i], shape=(), 
+                                      minval=-distances[i], 
+                                      maxval=distances[i]) 
+                        for i in range(dim)])
+    
+    # 创建带偏移的网格
+    for i in range(dim):
+        grid_i = jnp.linspace(mins[i], maxs[i], nums[i])[1:-1] + shifts[i]
+        grids.append(grid_i)
+    
+    # 使用mesh_flat创建网格点
+    return jnp.stack(mesh_flat(*grids), axis=-1).reshape(-1, dim)
+
+
+# def lhs_sampling(mins, maxs, num):
+#     lb = jnp.array(mins)
+#     ub = jnp.array(maxs)
+#     if not len(lb) == len(ub):
+#         raise ValueError(f"mins and maxs should have the same length.")
+#     ret = lhs(len(lb), int(num)) * (ub - lb) + lb
+#     # return [ret[:, :-1], ret[:, -1:]]
+#     return ret
+
+# In Jax
+# def shfted_grid(mins, maxs, num, key):
 #     if not len(mins) == len(maxs) == len(num):
 #         raise ValueError(f"mins, maxs, num should have the same length.")
     
-#     each_col = [torch.linspace(mins[i], maxs[i], num[i], device=DEVICE)[1:-1]
+#     each_col = [jnp.linspace(mins[i], maxs[i], num[i])[1:-1]
 #                 for i in range(len(mins))]
 #     distances = [(maxs[i] - mins[i]) / (num[i] - 1) for i in range(len(mins))]
-#     shift = [torch.tensor(np.random.uniform(-distances[i], distances[i], 1), device=DEVICE)
+#     shift = [random.uniform(key, (1,), minval=-distances[i], maxval=distances[i])
 #              for i in range(len(distances))]
-#     shift = torch.cat(shift, dim=0)
+#     shift = jnp.concatenate(shift, axis=0)
 #     each_col = [each_col[i] + shift[i] for i in range(len(each_col))]
-
-#     return torch.stack(torch.meshgrid(*each_col, indexing="ij"), axis=-1).reshape(-1, len(mins))
-
-
-# In Jax
-def shfted_grid(mins, maxs, num, key):
-    if not len(mins) == len(maxs) == len(num):
-        raise ValueError(f"mins, maxs, num should have the same length.")
-    
-    each_col = [jnp.linspace(mins[i], maxs[i], num[i])[1:-1]
-                for i in range(len(mins))]
-    distances = [(maxs[i] - mins[i]) / (num[i] - 1) for i in range(len(mins))]
-    shift = [random.uniform(key, (1,), minval=-distances[i], maxval=distances[i])
-             for i in range(len(distances))]
-    shift = jnp.concatenate(shift, axis=0)
-    each_col = [each_col[i] + shift[i] for i in range(len(each_col))]
-    return jnp.stack(mesh_flat(*each_col), axis=-1).reshape(-1, len(mins))
+#     return jnp.stack(mesh_flat(*each_col), axis=-1).reshape(-1, len(mins))
 
 if __name__ == "__main__":
-    mins = [0, 0]
-    maxs = [1, 1]
+    mins = jnp.array([0, 0])
+    maxs = jnp.array([1, 1])
     num = 100
-    # print(lhs_sampling(mins, maxs, num))
-    data = lhs_sampling(mins, maxs, num)
-    data2 = lhs_sampling(mins, maxs, num)
-    plt.scatter(data[:, 0], data[:, 1])
-    plt.scatter(data2[:, 0], data2[:, 1])
+    
+    # 使用JAX的随机数生成器
+    key = random.PRNGKey(42)
+    key1, key2 = random.split(key)
+    
+    # 生成样本
+    data = shifted_grid(mins, maxs, [20, 20], key1)
+    data2 = shifted_grid(mins, maxs, [20, 20], key2)
+    
+    # 可视化
+    plt.figure(figsize=(8, 8))
+    plt.scatter(data[:, 0], data[:, 1], alpha=0.6, label='Sample 1')
+    plt.scatter(data2[:, 0], data2[:, 1], alpha=0.6, label='Sample 2')
+    plt.legend()
+    plt.xlim(0, 1)
+    plt.ylim(0, 1)
+    plt.title(f'Latin Hypercube Sampling (n={num})')
     plt.show()
